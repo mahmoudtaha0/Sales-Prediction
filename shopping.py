81bc137464530eacc89e00a17d400c55c9b18cb2
mahmoudtaha0 <mahmoud.ahmed021@eng-st.cu.edu.eg>
2024-06-23 21:22:22 +0300

    Evaluation

--- shopping.py
+++ shopping.py
@@ -1,18 +1,10 @@
 import csv
-import sys
-
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 TEST_SIZE = 0.4
 
-
 def main():
-
-    # Check command-line arguments
-    # if len(sys.argv) != 2:
-    #     sys.exit("Usage: python shopping.py data")
-
     # Load data from spreadsheet and split into train and test sets
     evidence, labels = load_data("shopping.csv")
     # print(evidence)
@@ -34,33 +26,7 @@ def main():
 
 
 def load_data(filename):
-    """
-    Load shopping data from a CSV file `filename` and convert into a list of
-    evidence lists and a list of labels. Return a tuple (evidence, labels).
-
-    evidence should be a list of lists, where each list contains the
-    following values, in order:
-        - Administrative, an integer
-        - Administrative_Duration, a floating point number
-        - Informational, an integer
-        - Informational_Duration, a floating point number
-        - ProductRelated, an integer
-        - ProductRelated_Duration, a floating point number
-        - BounceRates, a floating point number
-        - ExitRates, a floating point number
-        - PageValues, a floating point number
-        - SpecialDay, a floating point number
-        - Month, an index from 0 (January) to 11 (December)
-        - OperatingSystems, an integer
-        - Browser, an integer
-        - Region, an integer
-        - TrafficType, an integer
-        - VisitorType, an integer 0 (not returning) or 1 (returning)
-        - Weekend, an integer 0 (if false) or 1 (if true)
-
-    labels should be the corresponding list of labels, where each label
-    is 1 if Revenue is true, and 0 otherwise.
-    """
+    # Label is revenue
     month_mapping = {
         "Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "June": 6,
         "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12
@@ -95,13 +61,10 @@ def load_data(filename):
           ])
           label.append(1 if row[17] == "TRUE" else 0)
     return evidence, label
+    raise NotImplementedError
 
 
 def train_model(evidence, labels):
-    """
-    Given a list of evidence lists and a list of labels, return a
-    fitted k-nearest neighbor model (k=1) trained on the data.
-    """
     model = KNeighborsClassifier(n_neighbors=1)
     model.fit(evidence, labels)
     return model
@@ -109,20 +72,25 @@ def train_model(evidence, labels):
 
 
 def evaluate(labels, predictions):
-    """
-    Given a list of actual labels and a list of predicted labels,
-    return a tuple (sensitivity, specificity).
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
 
-    Assume each label is either a 1 (positive) or 0 (negative).
+    for actual, predicted in zip(labels, predictions):
+        if actual == 1 and predicted == 1:
+            true_positive += 1
+        elif actual == 0 and predicted == 0:
+            true_negative += 1
+        elif actual == 1 and predicted == 0:
+            false_negative += 1
+        else: 
+            false_positive += 1
 
-    `sensitivity` should be a floating-point value from 0 to 1
-    representing the "true positive rate": the proportion of
-    actual positive labels that were accurately identified.
+    sensitivity = true_positive/(true_positive + false_negative) if (true_positive + false_negative) > 0 else 0
+    specificity = true_negative/(true_negative + false_positive) if (true_negative + false_positive) > 0 else 0
 
-    `specificity` should be a floating-point value from 0 to 1
-    representing the "true negative rate": the proportion of
-    actual negative labels that were accurately identified.
-    """
+    return sensitivity,specificity
     raise NotImplementedError
 
 
